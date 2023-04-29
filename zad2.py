4cb26f701b40003d1ca48bf7d9a0af510e9e7587
Kasia-q <kasia21bak@gmail.com>
2023-04-29 15:54:03 +0200

    first commit

---
+++ zad2.py
@@ -0,0 +1,4 @@
+sentence = input("Podaj swoje zdanie: ")
+words = sentence.split()
+words.reverse()
+print(' '.join(words))
---
+++ zad3.py
@@ -0,0 +1,3 @@
+sentence = input("Podaj zdanie: ")
+count = len(sentence.split())
+print(f"Liczba słów: {count}")
